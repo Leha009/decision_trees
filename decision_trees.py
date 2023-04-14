691f1614f825d3602df4be68679030097528053c
Leha009 <aleshay1@mail.ru>
2023-04-15 00:38:36 +0300

    trees improvements

--- decision_trees.py
+++ decision_trees.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import hashlib
+from datetime import datetime
 
 if len(sys.argv) != 4:
     print("python decision_trees.py {num of trees} {num of options} {question number Вопрос № XXX}")
@@ -10,11 +12,25 @@ NUM_OF_TREES = int(sys.argv[1])
 NUM_OF_OPTIONS = int(sys.argv[2])
 QUESTION_NUM = int(sys.argv[3])
 
+now = datetime.now()
+current_time = now.strftime("%H:%M")
+
+to_hash = f"DT_{NUM_OF_TREES}_{NUM_OF_OPTIONS}_{QUESTION_NUM}_{current_time}"
+res_of_hash = hashlib.md5(to_hash.encode())
+
+seed2 = int(res_of_hash.hexdigest(), 16)
+seed2 %= 2**32 - 1
+print(seed2, type(seed2))
+
 letters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 
 np.random.seed(QUESTION_NUM)
+npoptions = np.random.choice(a=NUM_OF_OPTIONS, size=NUM_OF_TREES//2)
 
-npoptions = np.random.choice(a=NUM_OF_OPTIONS, size=NUM_OF_TREES)
+np.random.seed(seed2)
+npoptions2 = np.random.choice(a=NUM_OF_OPTIONS, size=NUM_OF_TREES//2)
+
+npoptions = np.concatenate([npoptions, npoptions2], axis=0)
 
 options = [0 for _ in range(NUM_OF_OPTIONS)]
 
